ea8747b76a66206e83ee638ebf21ff951c7645dc
hantaotaohan <hantaotaohan@gmail.com>
2020-11-30 11:13:07 +0800

    taotao @  taotao in 2020-11-30=11:13:07

--- docs/static/js/wiki.js
+++ docs/static/js/wiki.js
@@ -16,14 +16,14 @@ $(function(){
         var html = $(".content-body").html();
         var toc_html = '<div class="toc">'+$(".toc").html() + "</div>"
         var content = html.replace(toc_html, "");
-        var html = '<div class="col-md-3">\n'+toc_html+'\n</div>\n';
-        html += '<div class="col-md-9">\n'+content+'\n</div>\n';
+        var html = '<div class="col-md-9">\n'+toc_html+'\n</div>\n';
+        html += '<div class="col-md-3">\n'+content+'\n</div>\n';
         html = html.replace(/blockquote/g, "pre");
         $(".content-body").html(html);
-        $(".toc").addClass("col-md-6 bd-toc sticky-top")
-        $(".toc").attr("role", "complementary"); 
+        $(".toc").addClass("bd-toc sticky-top")
+        /* $(".toc").attr("role", "complementary"); */
         $(".toc").attr("data-spy", "affix");
-        $(".toc").attr("data-offset-top", "200") 
+        /* $(".toc").attr("data-offset-top", "200") */
         var uls = $(".toc").find("ul");
         for (var i = 0; i < uls.length; i++){
             ul = uls[i];
