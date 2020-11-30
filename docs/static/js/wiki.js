5a2c4b7e7b14974caa992d33860c5337144ed571
hantaotaohan <hantaotaohan@gmail.com>
2020-11-30 10:56:49 +0800

    taotao @  taotao in 2020-11-30=10:56:49

--- docs/static/js/wiki.js
+++ docs/static/js/wiki.js
@@ -19,9 +19,23 @@ $(function(){
         var html = '<div class="col-md-3">\n'+toc_html+'\n</div>\n';
         html += '<div class="col-md-9">\n'+content+'\n</div>\n';
         html = html.replace(/blockquote/g, "pre");
-        $(".container").html(html);
-        $(".toc").addClass("bd-toc");
+        $(".content-body").html(html);
+        $(".toc").addClass("bs-sidebar");
+        /* $(".toc").attr("role", "complementary"); */
+        $(".toc").attr("data-spy", "affix");
+        /* $(".toc").attr("data-offset-top", "200") */
         var uls = $(".toc").find("ul");
+        for (var i = 0; i < uls.length; i++){
+            ul = uls[i];
+            console.log($(ul).parent(), $(".toc"));
+            console.log($(ul).parent() == $(".toc"));
+            console.log($(ul).parent().hasClass("toc"));
+            if ($(ul).parent().hasClass("toc"))
+                $(ul).addClass("nav bs-sidenav");
+            else
+                $(ul).addClass("nav");
+
+        }
     }
 });
 
