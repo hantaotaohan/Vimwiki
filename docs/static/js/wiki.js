80621efe736b9803c5e4cd0e839a0835753c2e7d
hantaotaohan <hantaotaohan@gmail.com>
2020-11-30 09:55:34 +0800

    taotao @  taotao in 2020-11-30=09:55:34

--- docs/static/js/wiki.js
+++ docs/static/js/wiki.js
@@ -14,7 +14,7 @@ $(function(){
     // 生成左侧toc导航
     if ($(".toc").html().trim()){
         var html = $(".container-fluid").html();
-        var toc_html = '<div class="toc">'+$(".toc").html() + "</div>"
+        var toc_html = '<nav class="toc">'+$(".toc").html() + "</div>"
         var content = html.replace(toc_html, "");
         var html = '<div class="row flex-xl-nowrap"\n'+toc_html+'\n</div>\n';
         html += '<div class="col-md-9">\n'+content+'\n</div>\n';
