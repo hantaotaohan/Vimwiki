a2596a3fcfa11b32f84c9813fba825dbc15b494a
hantaotaohan <hantaotaohan@gmail.com>
2020-11-30 09:57:09 +0800

    taotao @  taotao in 2020-11-30=09:57:09

--- docs/static/js/wiki.js
+++ docs/static/js/wiki.js
@@ -20,7 +20,7 @@ $(function(){
         html += '<div class="col-md-9">\n'+content+'\n</div>\n';
         html = html.replace(/blockquote/g, "pre");
         $(".container-fluid").html(html);
-        $(".toc").addClass("bd-toc sticky-top");
+        $(".toc").addClass("nav class="bd-toc sticky-top");
         /* $(".toc").attr("role", "complementary"); */
         $(".toc").attr("data-spy", "affix");
         /* $(".toc").attr("data-offset-top", "200") */
